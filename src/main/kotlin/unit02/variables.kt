919a130fda1919a1ef96416d2779328066d5aeee
steph <e3521792@student.dcccd.edu>
2023-04-21 02:12:08 -0500

    Created variables

---
+++ src/main/kotlin/unit02/variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val myAge: Int = 23
+    var averageAge: Double = (myAge + 30) / 2.0
+    val testNumber: Int = 77
+    val even0dd: Int = testNumber % 2
+    var answer: Int = 0
+    answer += 1
+    answer += 10
+    answer *= 10
+    answer = answer shr 3
+
+    println(myAge)
+    println(averageAge)
+    println(testNumber)
+    println(answer)
+
+}
