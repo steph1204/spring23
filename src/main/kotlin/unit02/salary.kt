b515fc1f42ec9165fe6db93d1da9c8de67ad0edc
steph <e3521792@student.dcccd.edu>
2023-04-21 03:41:40 -0500

    created salary example

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,10 @@
+package unit02
+
+fun  main() {
+
+    val name = "Stephanie Hernandez"
+    val age = 23
+    val annualPay = 70000.0
+
+    println("My name is $name, my age is $age, and I hope to earn $$annualPay per year.")
+}
