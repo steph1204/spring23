e0770b685c47f71622f3644528c5323f2926b158
steph <e3521792@student.dcccd.edu>
2023-04-21 00:04:46 -0500

    created var example

---
+++ src/main/kotlin/unit02/varSamples.kt
@@ -0,0 +1,16 @@
+fun main () {
+    var name = "Stephanie"
+    var num = 7
+    var numLetter = "12"
+
+    println(name)
+    println(num)
+    println(numLetter)
+
+    println("Your number times 2 is " + num*2)
+    println("The other number times 2 is " + (num-1)*2)
+
+
+
+
+}
