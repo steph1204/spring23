5d318ea35c2f67658635178ce5e3f240789c9bee
steph <e3521792@student.dcccd.edu>
2023-04-23 01:01:38 -0500

    .

---
+++ src/main/kotlin/unit03/romans.kt
@@ -0,0 +1,43 @@
+package unit03
+
+fun main() {
+
+    print("Enter a number in the range of 1 - 10: ")
+
+
+    if (readln().toInt()
+        == 1
+    ) {
+        print("The Roman numeral is I")
+    }
+    else if (readln().toInt() == 2) {
+        print("The Roman numeral is II")
+    }
+    else if (readln().toInt() == 3) {
+        print("The Roman numeral is III")
+    }
+    else if (readln().toInt() == 4) {
+        print("The Roman numeral is IV")
+    }
+    else if (readln().toInt() == 5) {
+        print("The Roman numeral is V")
+    }
+    else if (readln().toInt() == 6) {
+        print("The Roman numeral is VI")
+    }
+    else if (readln().toInt() == 7) {
+        print("The Roman numeral is VII")
+    }
+    else if (readln().toInt() == 8) {
+        print("The Roman numeral is VII")
+    }
+    else if (readln().toInt() == 9) {
+        print("The Roman numeral is IX")
+    }
+    else if (readln().toInt() == 10) {
+        print("The Roman numeral is 10")
+    }
+
+
+
+}
