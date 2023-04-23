fba17d69dab8698b1943b1df19175e0a0fe4c582
steph <e3521792@student.dcccd.edu>
2023-04-23 02:01:35 -0500

    created test

---
+++ src/main/kotlin/unit03/test.kt
@@ -0,0 +1,11 @@
+package unit03
+
+fun main () {
+    val number = 5
+
+    if (number > 0) {
+        println("$number is positive")
+    } else {
+        println("$number is non-positive")
+    }
+}
